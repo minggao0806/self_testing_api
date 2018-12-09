62c910088d553c2a09b5acf0555d4bc69ea76400
gxm <74994608@qq.com>
2018-12-09 20:37:18 +0800

    项目完成

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+var config = {
+    mysqlOption : {
+        host:'127.0.0.1',
+        port : '3306',
+        user : "root",
+        password : "gfyyzs1378436",
+        database : '1609b'
+    }
+}
+module.exports = config;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+let {query} = require('../config/mysqlContent')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api/draw', function(req, res, next) {
+  var sql = `select count(*) from zice2`;
+  let {count,pageSize} = req.query;
+  var obj = {};
+  query(sql).then((rows) => {
+    console.log(rows);
+    obj.total = rows[0]['count(*)'];
+    
+  })
+  var start = (count-1)*pageSize;
+  var sql1 = `select * from zice2 limit ${start},${pageSize}`;
+  query(sql1).then((rows) => {
+    obj.data = rows;
+    res.send(obj);
+  })
+
 });
 
 module.exports = router;
